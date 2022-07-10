a92069f5b466cb4de4f52e92932bebc0fc39951a
Kelvin-MKL <109009865+Kelvin-MKL@users.noreply.github.com>
2022-07-10 17:23:22 +1000

    deleted unnecessary files

--- table-with-nested-value-demo/src/App.js
+++ table-with-nested-value-demo/src/App.js
@@ -1,26 +1,20 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import _ from "lodash";
 import "./App.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    dishes: [],
+    cuisine: [],
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
